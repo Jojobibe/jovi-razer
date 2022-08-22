96ecf4d2a28e2fd812a9c5204fba80c53d235c50
Jovillene <japenalosa33@gmail.com>
2022-08-22 19:32:48 +0800

    update

--- app.js
+++ app.js
@@ -102,8 +102,8 @@ moreItems.addEventListener("click", (e) => {
   items.classList.toggle("show-more");
 
   if (moreItems.innerHTML === "VIEW ALL PRODUCTS") {
-    moreItems.innerHTML = "CLOSE";
-  } else {
     moreItems.innerHTML = "VIEW ALL PRODUCTS";
+  } else {
+    moreItems.innerHTML = "CLOSE";
   }
 });
