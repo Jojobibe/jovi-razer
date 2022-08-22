fe39987f92e16b06fccd87f03eec7ec321c60010
Jovillene <japenalosa33@gmail.com>
2022-08-22 21:49:52 +0800

    update

--- app.js
+++ app.js
@@ -51,6 +51,22 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+function openNav2() {
+  document.getElementById("myNav2").style.width = "100%";
+}
+
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
+
+function openNav3() {
+  document.getElementById("myNav3").style.width = "100%";
+}
+
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0%";
+}
+
 // contact
 
 function validate() {
